ca993da8c6dc4223323e5ea46370cbdd0165899a
Vanish-Zeng <957589005@qq.com>
2022-04-13 21:31:04 +0800

    配置store：一些全局变量设置。😎

---
+++ src/store/main.js
@@ -0,0 +1,14 @@
+export default {
+    state: {
+        isLogin: false,
+        searchWord: '',
+    },
+    getters: {
+        isLogin: state => state.isLogin,
+        searchWord: state => state.searchWord,
+    },
+    mutations: {
+        SET_IS_LOGIN: (state, isLogin) => { state.isLogin = isLogin },
+        SET_SEARCH_WORD: (state, searchWord) => { state.searchWord = searchWord }
+    }
+}
---
+++ src/store/user.js
@@ -0,0 +1,18 @@
+export default {
+    state: {
+        userInfo: window.localStorage.getItem('userInfo'),
+        id: '',
+        nickname: '',
+        avatar: '',
+    },
+    getters: {
+        id: state => state.id,
+        nickname: state => state.nickname,
+        avatar: state => state.avatar,
+    },
+    mutations: {
+        SET_ID: (state, id) => { state.id = id },
+        SET_NICKNAME: (state, nickname) => { state.nickname = nickname },
+        SET_AVATAR: (state, avatar) => { state.avatar = avatar }
+    }
+}
