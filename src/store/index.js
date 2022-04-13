1bc6f7fe084209a057a0270b137e77e99f7cb323
Vanish-Zeng <957589005@qq.com>
2022-04-13 21:30:12 +0800

    路由跳转：前置路由bug修复。😢

--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,6 @@
 import { createStore } from 'vuex'
+import user from "@/store/user";
+import main from "@/store/main";
 
 export default createStore({
   state: {
@@ -10,5 +12,7 @@ export default createStore({
   actions: {
   },
   modules: {
+    user,
+    main
   }
 })
