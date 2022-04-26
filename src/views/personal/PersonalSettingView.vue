e1f5daef53d28130e8da363263982c2a64dc084d
Vanish-Zeng <957589005@qq.com>
2022-04-26 15:12:12 +0800

    修复页面刷新、回退，菜单激活单元错误的问题。⚙

--- src/views/personal/PersonalSettingView.vue
+++ src/views/personal/PersonalSettingView.vue
@@ -5,29 +5,31 @@
         <div class="setting-font">设置</div>
         <el-divider/>
         <el-menu
+            @select="handleIndexChange"
             :default-active="activeIndex"
             class="setting-menu-first"
         >
-          <el-menu-item index="info">
+          <el-menu-item index="settingPersonalInfo">
             <el-icon><user /></el-icon>
             <span>个人资料</span>
           </el-menu-item>
 
-          <el-menu-item index="account">
+          <el-menu-item index="settingAccount">
             <el-icon><setting /></el-icon>
             <span>账号设置</span>
           </el-menu-item>
         </el-menu>
 
         <el-menu
+            @select="handleIndexChange"
             :default-active="activeIndex"
             class="setting-menu-second"
         >
-          <el-menu-item index="info">
+          <el-menu-item index="settingPersonalInfo">
             <el-icon><user /></el-icon>
           </el-menu-item>
 
-          <el-menu-item index="account">
+          <el-menu-item index="settingAccount">
             <el-icon><setting /></el-icon>
           </el-menu-item>
         </el-menu>
@@ -44,20 +46,35 @@
 </template>
 
 <script>
-import {ref} from "vue";
+import {ref, watch} from "vue";
+import {useRouter} from "vue-router";
 
 export default {
   name: "PersonalSettingView",
   setup() {
-    let activeIndex = ref('info')
-    const changeIndex = () => {
-      if (activeIndex.value === 'info') activeIndex.value = 'account'
-      else activeIndex.value = 'info'
+    const router = useRouter()
+    let activeIndex = ref('settingPersonalInfo')
+    const handleIndexChange = (index) => {
+      if (index === 'settingPersonalInfo') {
+        activeIndex.value = 'settingPersonalInfo'
+        router.push({ name: 'settingPersonalInfo' })
+      } else {
+        activeIndex.value = 'settingAccount'
+        router.push({ name: 'settingAccount' })
+      }
     }
+
+    /**
+     * 监听路由名称的变化，页面刷新时立即生效，用于更新菜单激活单元
+     * 解决页面刷新或则回退，菜单激活单元不变的问题
+     */
+    watch(() => router.currentRoute.value.name,(newValue,oldValue)=> {
+      activeIndex.value = newValue
+    },{ immediate: true })
 
     return {
       activeIndex,
-      changeIndex
+      handleIndexChange
     }
   }
 }
@@ -71,12 +88,6 @@ export default {
     font-size: 1.5rem;
     text-align: center;
   }
-}
-
-.setting-border {
-  //border-style:solid;
-  //border-color: #d7d0d0;
-  //border-width: 1px;
 }
 
 .setting-menu-first {
